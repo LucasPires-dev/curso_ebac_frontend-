4699bb34340f439e05c42291873f90540ba66930
Lucas Pires <lcpires7.dev@gmail.com>
2024-08-27 15:01:01 -0300

    atualização das validações do formulário

--- main.js
+++ main.js
@@ -4,28 +4,25 @@ const verificaNumeros = (e) => {
     const valorA = document.getElementById('numeroA')
     const valorB = document.getElementById('numeroB')
     const resultado = document.getElementById('resultado')
-    resultado.innerHTML = (`O número <b>${valorA}</b> é maior que o número <b>${valorB}</b>`)
     const AMaior = parseInt(valorA.value) > parseInt(valorB.value)
     const AIgualB = parseInt(valorA.value) == parseInt(valorB.value)
-    console.log(AMaior)
-    console.log(AIgualB)
+    const error = document.getElementsByClassName('error')[0]
     if(AMaior){
-       resultado.style = ""
-        resultado.innerHTML = (`O número <b>A</b> é maior que o número <b>B</b>.`)
-        resultado.style.backgroundColor = "green"
-        resultado.style.color = "white"
+        error.style = ""
+        resultado.style = ""
+        error.innerHTML = "O número A é maior que o B!"
+        error.style.display = "block"
     } else if (!AMaior && !AIgualB) {
+        error.style = ""
         resultado.style = ""
-        resultado.innerHTML = (`O número <b>B</b> é maior que o número <b>A</b>.`)
-        resultado.style.backgroundColor = "red"
-        resultado.style.color = "white"
-    } else if(AIgualB || valorA === NaN && valorB === NaN) {
+        resultado.innerHTML = (`Tudo Certo!`)
+        resultado.style.display ='block'
+    } else if(AIgualB) {
+        error.style = ""
         resultado.style = ""
-        resultado.style.backgroundColor = "red"
-        resultado.style.color = "white"
-        resultado.innerHTML = "Os valores de A e B são iguais!"
+        error.innerHTML = "Os valores de A e B são iguais!"
+        error.style.display = "block"
     }
-    resultado.style.display ='block'
 }
 
 const submit = document.getElementById('form').addEventListener('submit', verificaNumeros)
