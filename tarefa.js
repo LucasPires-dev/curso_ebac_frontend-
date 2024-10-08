5a4df72d95e11fa179224555162cdabc857cd878
Lucas Pires <lcpires7.dev@gmail.com>
2024-10-07 23:38:19 -0300

    Primeiro commit

---
+++ tarefa.js
@@ -0,0 +1,9 @@
+"use strict";
+const multiplicacao = (num1, num2) => {
+    return num1 * num2;
+};
+console.log(multiplicacao(1, 3));
+const printNome = (nome) => {
+    return 'Olá ' + nome + '!';
+};
+console.log(printNome('Lucas'));
---
+++ tarefa.ts
@@ -0,0 +1,11 @@
+const multiplicacao = (num1: number, num2: number): number =>{
+    return num1 * num2
+}
+
+console.log(multiplicacao(1, 3))
+
+const printNome = (nome: string): string =>{
+    return 'Olá '+ nome + '!'
+}
+
+console.log(printNome('Lucas'))
